7c8746dab147ad8a18ad3e323e4149af9c15850b
dobbyistatis <mypoodle@naver.com>
2021-11-01 16:19:13 +0900

    create template, habit-tracker file

---
+++ habit-tracker/src/reportWebVitals.js
@@ -0,0 +1,16 @@
+//serviceworker.js is used to  provide offline services
+//In this React version, there is a report web instead of a service worker.
+
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
